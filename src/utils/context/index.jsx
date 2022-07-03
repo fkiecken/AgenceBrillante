075117958fd9b87faa70e247fe2c62dbec794dd9
fkiecken <fkiecken@gmail.com>
2022-07-03 21:05:19 +0200

    fix conflits

--- src/utils/context/index.jsx
+++ src/utils/context/index.jsx
@@ -1,8 +1,6 @@
 import { createContext, useState } from 'react'
 
 export const ThemeContext = createContext()
-export const ResultSurveyContext = createContext()
-
 
 export const ThemeProvider = ({ children }) => {
   const [theme, setTheme] = useState('light')
@@ -14,45 +12,5 @@ export const ThemeProvider = ({ children }) => {
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
       {children}
     </ThemeContext.Provider>
-  )
-}
-
-export const SurveyProdvider = () => {
-  const resultSurvey = [
-    {
-      questionNumber: 1,
-      answer : null,
-    },
-    {
-      questionNumber: 2,
-      answer : null,
-    },    
-    {
-      questionNumber: 3,
-      answer : null,
-    },    
-    {
-      questionNumber: 4,
-      answer : null,
-    },    
-    {
-      questionNumber: 5,
-      answer : null,
-    },    
-    {
-      questionNumber: 6,
-      answer : null,
-    },
-  ]
-/*
-  const toggleResultSurvey = (QuestionNumber, ResultAnswer) => {
-    resultSurvey.findIndex(QuestionNumber)(object => {
-      object.Answer = ResultAnswer
-    })
-  }
-*/
-  return (
-    <SurveyProdvider.Provider>
-    </SurveyProdvider.Provider>
   )
 }
